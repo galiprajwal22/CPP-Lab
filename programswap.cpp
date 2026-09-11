6d4b018ae1005b7dedc22407b2ad9105bed9ee8e
galiprajwal22 <galiprajwal2@gmail.com>
2026-09-11 11:10:01 +0530

    Add files via upload

---
+++ programswap.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void swapRef(int &a, int &b) {int t=a; a=b; b=t;} 
+void swapPtr(int *a, int *b) {int t=*a; *a=*b; *b=t;}
+
+int main() {
+    int x=10, y=20;
+    swapRef(x,y);
+    cout<<"After swarRef: x=" << x << "y=" << y << endl;
+    swapPtr(&x,&y);
+    cout << "After swarPtr: x=" << x << "y=" << y << endl;
+
+    int &alias=x;
+    alias=99;
+    cout << "x via alias=" << x << endl;
+    
+    return 0;
+}
